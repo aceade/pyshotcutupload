d48f4a17cd485017efce5596618c6debb02052d7
aceade@github.com <philiprowlands90@gmail.com>
2022-08-25 20:08:26 +0100

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # pyshotcutupload
-A Python utility for uploading Shotcut projects to a server for rendering/exporting
+A Python utility for uploading Shotcut projects to a server for rendering/exporting. 
+
+(Shotcut)[https://shotcut.org/] is an open-source, cross-platform video editor built upon the (MLT)[https://www.mltframework.org/] framework. This was originally written so I could reuse a Linux Mint desktop as a rendering/export server for Shotcut, along with getting some further experience with Python.
+
+## Requirements
+A server with Shotcut installed. SSH must be enabled on this and set to use SSH keys.
+Python 3.8.10 or later
+
+## Installation
+`pip install -r requirements.txt`
+
+### SSH setup
+If SSH is _not_ set up on your server (e.g. you are repurposing an old desktop) or does not use SSH keys, set up SSH as follows:
+1. Set your server to have a static IP address. Doing so is beyond the scope of this guide.
+2. Enable SSH on the server by running `systemctl enable sshd`. Optionally, set it to use a port other than 22.
+3. Create an SSH key by running `ssh-keygen` on your local computer. 
+	- Take note of the path to the file; this utility expects the filename to be "id_rsa".
+	- Fill out the passphrase. Take note of this for later.
+4. Copy this to your server by running `ssh-copy-id user@server`, e.g. `ssh-copy-id admin@1.2.3.4`. When prompted for a password, use the password associated with the remote server. Do *not* use the passphrase for the SSH key.
+
+## How to use this
+
+### Configuration file
+Copy the config.example.yml file to config.yml and update as follows:
+1. Replace the username field with the username you will use to access the server
+2. Replace the host with the IP address or hostname of the server.
+3. If your server uses a different port to 22 (the standard SSH/SFTP port), replace the port entry with the expected value.
+4. Replace the path in the key_path variable with the actual path to the folder that contains the `id_rsa` file. In most cases, this will be `$HOME/.ssh`.
+5. Replace the key_pass variable with the passphrase defined above.
+
+### Running
+`python uploader.py path/to/mlt_file path/to/remote_dir`
+- The mtl_file is the format in which Shotcut saves projects.
+- The remote_dir is the remote directory to which to upload them, relative to the remote user's home directory (e.g. Videos/Test instead of /home/admin/Videos/Test)
+
+### Troubleshooting
+TODO
+
+## Licence
+MIT (see the (LICENCE)[./LICENCE] file).
---
+++ uploader.py
@@ -0,0 +1,111 @@
+import argparse
+import subprocess
+import logging
+from paramiko.client import SSHClient
+from pathlib import PurePath
+import xml.etree.cElementTree as et
+from yaml import load, Loader
+
+def check_server_up(host):
+    logging.info("Issuing ping to %s", host)
+    proc = subprocess.run(["ping", "-c", '2', host])
+    return proc.returncode == 0
+    
+def upload_files(directory, remote_dir, config):
+    username = config["username"]
+    host = config["host"]
+    port = config["port"] or 22
+    private_key = str(config["key_path"]) + "/id_rsa"
+    private_key_pass = config["key_pass"]
+    logging.info("Uploading to %s", remote_dir)
+    
+    client = SSHClient()
+    client.load_system_host_keys()
+    client.connect(host, port=port, username=username, key_filename=private_key, passphrase=private_key_pass)
+    sftp = client.open_sftp()
+    
+    # Check if the folder exists. If not, create it
+    try:
+        sftp.listdir(remote_dir)
+    except IOError:
+        sftp.mkdir(remote_dir)
+    
+    sftp.chdir(remote_dir)
+    for file in directory:
+        logging.info("Uploading %s", file)
+        # The remotepath should contain the file name. If the forward slash is omitted, it becomes e.g. AAutomationTestsadtrombone.wav
+        sftp.put(localpath=file, remotepath=remote_dir + '/' + file, confirm=True)
+    client.close()
+    logging.info("All files uploaded")
+
+def get_resource_paths(file):
+    paths = []
+    tree = et.parse(file)
+    root = tree.getroot()
+    for el in root.iter("chain"):
+        for property in el.iter("property"):
+            # The resource attribute 
+            if property.attrib['name'] == 'resource':
+                paths.append(property.text)
+    return paths
+
+def clean_paths(file, res_paths):
+    cleaned_paths = []
+    tree = et.parse(file)
+    root = tree.getroot()
+    for el in root.iter("chain"):
+        for property in el.iter("property"):
+            if property.attrib['name'] == 'resource' and property.text in res_paths:
+                cleaned = property.text[(property.text.rindex('/'))+1:]
+                cleaned_paths.append(cleaned)
+                property.text = cleaned
+    tree.write(file.replace('.mlt', '_fixed.mlt'))
+    return cleaned_paths
+
+def get_resource_files(file):
+    res_paths = get_resource_paths(file)
+    print(res_paths)
+    cur_dir = PurePath(".")
+    for path in res_paths:
+        ## Account for WSL. There is probably a better way to do this
+        fp = PurePath(path.replace('C:', '/mnt/c').replace('D:', '/mnt/d'))
+        subprocess.run(["cp", fp, "."])
+    return clean_paths(file, res_paths)
+
+def get_config():
+    config = None
+    with open("config.yml") as file:
+        #stream = file("config.yml", "r")
+        config = load(file, Loader=Loader)
+    return config
+
+def loop_server(host):
+    attempts = 0
+    while check_server_up(host) is False:
+        attempts += 1
+    if (attempts >= 10):
+        return False
+    else:
+        return True
+
+parser = argparse.ArgumentParser(description="Upload files")
+parser.add_argument("file", type=str, help="The path to the MLT file")
+parser.add_argument("upload_dir", type=str, help="Where to upload it to, relative to the user's home directory")
+
+args = parser.parse_args()
+mlt_file = args.file
+remote_dir = args.upload_dir
+
+logging.basicConfig(level=logging.DEBUG)
+config = get_config()
+host = config["host"]
+
+if (loop_server(host) is True):
+    logging.info("Server is up, we shall proceed")
+    files = get_resource_files(mlt_file)
+    logging.info("Files: %s", files)
+    files.append("AutomationTest_fixed.mlt")
+    logging.info("Files are now: %s", files)
+    upload_files(files, str("/home/" + config["username"] + "/" + remote_dir), config)
+else:
+    logging.warning("The server is not up yet!")
