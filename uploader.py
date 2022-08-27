7e9a1f39f76e8a693674e3af0eeb5775601974f5
Philip Rowlands <philiprowlands90@gmail.com>
2022-08-27 11:44:04 +0100

    #7 cleaning up code

--- uploader.py
+++ uploader.py
@@ -1,16 +1,16 @@
 import argparse
 import subprocess
 import logging
-from paramiko.client import SSHClient
 from pathlib import Path, PurePath
 import xml.etree.cElementTree as et
+from paramiko.client import SSHClient
 from yaml import load, Loader
 
-def check_server_up(host):
-    logging.info("Issuing ping to %s", host)
-    proc = subprocess.run(["ping", "-c", '2', host])
+def check_server_up(address):
+    logging.info("Issuing ping to %s", address)
+    proc = subprocess.run(["ping", "-c", '2', address], check=True)
     return proc.returncode == 0
-    
+
 def upload_files(directory, remote_dir, config):
     username = config["username"]
     host = config["host"]
@@ -18,23 +18,25 @@ def upload_files(directory, remote_dir, config):
     private_key = str(config["key_path"]) + "/id_rsa"
     private_key_pass = config["key_pass"]
     logging.info("Uploading to %s", remote_dir)
-    
+
     client = SSHClient()
     client.load_system_host_keys()
-    client.connect(host, port=port, username=username, key_filename=private_key, passphrase=private_key_pass)
+    client.connect(host, port=port, username=username,
+                   key_filename=private_key, passphrase=private_key_pass)
     sftp = client.open_sftp()
-    
+
     # Check if the folder exists. If not, create it
     try:
         sftp.listdir(remote_dir)
     except IOError:
         sftp.mkdir(remote_dir)
-    
+
     sftp.chdir(remote_dir)
     try:
         for file in directory:
             logging.info("Uploading %s", file)
-            # The remotepath should contain the file name. If the forward slash is omitted, it becomes e.g. AAutomationTestsadtrombone.wav
+            # The remotepath should contain the file name.
+            # If the forward slash is omitted, it becomes e.g. AAutomationTestsadtrombone.wav
             sftp.put(localpath=file, remotepath=remote_dir + '/' + file, confirm=True)
         logging.info("All files uploaded")
     except FileNotFoundError as e:
@@ -46,11 +48,11 @@ def get_resource_paths(file):
     paths = []
     tree = et.parse(file)
     root = tree.getroot()
-    for el in root.iter("chain"):
-        for property in el.iter("property"):
+    for element in root.iter("chain"):
+        for prop in element.iter("property"):
             # The resource attribute is what contains the path to the file
-            if property.attrib['name'] == 'resource':
-                paths.append(property.text)
+            if prop.attrib['name'] == 'resource':
+                paths.append(prop.text)
     return paths
 
 def clean_paths(file, res_paths):
@@ -58,12 +60,12 @@ def clean_paths(file, res_paths):
     cleaned_paths = []
     tree = et.parse(file)
     root = tree.getroot()
-    for el in root.iter("chain"):
-        for property in el.iter("property"):
-            if property.attrib['name'] == 'resource' and property.text in res_paths:
-                cleaned = property.text[(property.text.rindex('/'))+1:]
+    for element in root.iter("chain"):
+        for prop in element.iter("property"):
+            if prop.attrib['name'] == 'resource' and prop.text in res_paths:
+                cleaned = prop.text[(prop.text.rindex('/'))+1:]
                 cleaned_paths.append(cleaned)
-                property.text = cleaned
+                prop.text = cleaned
 
     # Save to a new file with all paths adjusted to be relative to the project
     tree.write(cleaned_file)
@@ -75,12 +77,12 @@ def get_resource_files(file):
     for path in res_paths:
         if path.startswith("/") is False:
             # accounting for relative paths
-            fp = PurePath(file.replace(str(PurePath(file).name), '') + "/" + path)
+            file_path = PurePath(file.replace(str(PurePath(file).name), '') + "/" + path)
         else:
             # Account for WSL. There is probably a better way to do this
-            fp = PurePath(path.replace('C:', '/mnt/c').replace('D:', '/mnt/d'))
-        logging.info(fp)
-        subprocess.run(["cp", "-r", fp, "."])
+            file_path = PurePath(path.replace('C:', '/mnt/c').replace('D:', '/mnt/d'))
+        logging.info(file_path)
+        subprocess.run(["cp", "-r", file_path, "."], check=True)
     return clean_paths(file, res_paths)
 
 def get_config():
@@ -101,24 +103,28 @@ def loop_server(host):
 
     return is_up
 
-parser = argparse.ArgumentParser(description="Upload files")
-parser.add_argument("file", type=str, help="The path to the MLT file")
-parser.add_argument("upload_dir", type=str, help="Where to upload it to, relative to the user's home directory")
+def main():
+    parser = argparse.ArgumentParser(description="Upload files")
+    parser.add_argument("file", type=str, help="The path to the MLT file")
+    parser.add_argument("upload_dir", type=str,
+                        help="Where to upload it to, relative to the user's home directory")
 
-args = parser.parse_args()
-mlt_file = args.file
-remote_dir = args.upload_dir
+    args = parser.parse_args()
+    mlt_file = args.file
+    remote_dir = args.upload_dir
 
-logging.basicConfig(level=logging.DEBUG)
-config = get_config()
-host = config["host"]
+    logging.basicConfig(level=logging.DEBUG)
+    config = get_config()
+    host = config["host"]
 
-if (loop_server(host) is True):
-    logging.info("Server is up, we shall proceed")
-    files = get_resource_files(mlt_file)
-    # Copy the "cleaned" file to the local directory. TODO: remove all temp files
-    subprocess.run(["cp", mlt_file.replace(".mlt", "_fixed.mlt"), "."])
-    logging.info("Files: %s", files)
-    upload_files(files, str("/home/" + config["username"] + "/" + remote_dir), config)
-else:
-    logging.warning("The server is not up yet!")
+    if loop_server(host) is True:
+        logging.info("Server is up, we shall proceed")
+        files = get_resource_files(mlt_file)
+        # Copy the "cleaned" file to the local directory. TODO: remove all temp files
+        subprocess.run(["cp", mlt_file.replace(".mlt", "_fixed.mlt"), "."], check=True)
+        logging.info("Files: %s", files)
+        upload_files(files, str("/home/" + config["username"] + "/" + remote_dir), config)
+    else:
+        logging.warning("The server is not up yet!")
+
+main()
